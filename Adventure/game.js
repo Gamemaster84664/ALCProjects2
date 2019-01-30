f034218ada6b372f559aac08573a8c0fbac13db2
Gabriel <gamemaster84664@gmail.com>
2019-01-30 10:30:21 -0700

    attempted plot

--- Adventure/game.js
+++ Adventure/game.js
@@ -2,43 +2,122 @@ Game();
 
 function Game() {
 
+    //Starts the whole Game.
     alert("You are awake.");
     var playerName = prompt("What is your name?");
     alert("Welcome to Dream Simulator: Text Edition, " + playerName + ".");
-    FirstMorning();
+    FirstMorningBed();
     
-    function FirstMorning() {
+    
+    //First Morning
+    function FirstMorningBed() {
+        //First Prompt
         alert("The smell of bacon and eggs wafts its way upstairs to your room.");
         var inBed = prompt("Do you leave your bed? \n - Yes \n - No").toLowerCase();
         
         if(inBed == "yes" || inBed == "y") {
-            var outBed = prompt("You stand half naked in your room. Thankfully, your door is closed. \n - Get dressed \n - Open door").toLowerCase();
+            FirstMorningRoom();
             
-            if(outBed == "get dressed" || outBed == "dress" || outBed == "g") {
-                var dressed = prompt("You finally put clothes on. Your door is still closed. \n - Open door").toLowerCase();
-                
-                if(dressed == "open door" || dressed == "open" || dressed == "o") {
+            function FirstMorningRoom() {
+                //Naked or Dressed prompt
+                var outBed = prompt("You stand half naked in your room. Thankfully, your door is closed. \n - Get dressed \n - Open door").toLowerCase();
+
+                if(outBed == "get dressed" || outBed == "dress" || outBed == "g") {
+                    FirstMorningDressPrompt();
+                    
+                    function FirstMorningDressPrompt() {
+                        var dressed = prompt("You finally put clothes on. Your door is still closed. \n - Open door").toLowerCase();
+
+                        if(dressed == "open door" || dressed == "open" || dressed == "o") {
+                            alert("Your door swings open.");
+                            alert("The smell of delicious typical morning food intensifies.");
+                            FirstMorningDressed();
+                        }
+                        else {
+                            alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                            FirstMorningDressPrompt();
+                        }
+                    }
+                }
+
+                else if(outBed == "open door" || outBed == "open" || outBed == "o") {
                     alert("Your door swings open.");
-                    alert("The smell of delicous typical morning food intensifies.");
+                    alert("The smell of delicious typical morning food intensifies.");
+                    FirstMorningNaked();
+                }
+
+                else {
+                    alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                    FirstMorningRoom();
                 }
             }
         }
         
         else if(inBed == "no" || inBed == "n") {
-            alert("You stay in bed. The smell changes from breakfast to rotting flesh. Something is running up the stairs at a rapid pace.");
-            alert("Your door explodes open, revealing a shuddering corpse standing in the doorway.");
-            alert("It sprints across your room until it is standing above you, breathing hot, moist air into your face. You close your eyes as its jagged, yellow teeth slowly sink into your forehead.");
-            alert("Everything goes black.");
-            var restart = prompt("You have died. \n - Press R to restart.").toLowerCase();
+            alert("You wait another ten minutes in bed. Nothing happens.");
+            FirstMorningBed();
+        }
+        
+        else {
+            alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+            FirstMorningBed();
+        }
+    }
+    
+    /*var restart = prompt("You have died. \n - Press R to restart.").toLowerCase();
             
             if(restart == "r") {
                 Game();
             }
+    */
+    
+    function FirstMorningDressed() {
+        alert("There are no other rooms open upstairs.");
+        FirstMorningDownstairsDressed();
+    }
+    
+    
+    function FirstMorningNaked() {
+        FirstUpstairsPrompt();
+        
+        function FirstUpstairsPrompt() {
+            var upstairs = prompt("There is one other door open upstairs. \n - Enter room \n - Go downstairs").toLowerCase();
             
-        }
-        else {
-            alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
-            FirstMorning();
+            if(upstairs == "enter room" || upstairs == "enter" || upstairs == "e") {
+                FirstGreenBathroom();
+                
+                function FirstGreenBathroom() {
+                    var greenBathroom = prompt("You enter a small, green bathroom. There is a mirror over the sink. \n - Look at mirror \n - Leave room").toLowerCase();
+
+                    if(greenBathroom == "look at mirror" || greenBathroom == "look") {
+                        FirstUpstairsMirror();
+                        
+                        function FirstUpstairsMirror() {
+                            var greenBathroomPrompt = prompt("You look shamefully at your awkward, undressed self in the mirror. \n - Go get dressed \n - Screw you, narrator!");
+                            
+                        }
+                    }
+                    
+                    else if(greenBathroom == "leave room" || greenBathroom == "leave") {
+                        alert("You refuse to look at your undressed self in the mirror. Since there's nothing else to do, you head downstairs.");
+                        FirstMorningDownstairsNaked();
+                    }
+                    
+                    else {
+                        alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                        FirstGreenBathroom();
+                    }
+                }
+            }
+            
+            else if(upstairs == "go downstairs" || upstairs == "go" || upstairs == "downstairs" || upstairs == "g") {
+                FirstMorningDownstairsNaked();
+            }
+            
+            else {
+                alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                FirstUpstairsPrompt();
+            }
         }
     }
 }
