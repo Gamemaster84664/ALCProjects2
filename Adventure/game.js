914fdc5adbd061d43c5b4390bcbe88ab561cc793
Gabriel <gamemaster84664@gmail.com>
2019-02-05 10:29:47 -0700

    started first drem

--- Adventure/game.js
+++ Adventure/game.js
@@ -6,7 +6,10 @@ function Game() {
     alert("You are awake.");
     var playerName = prompt("What is your name?");
     alert("Welcome to Dream Simulator: Text Edition, " + playerName + ".");
+    confirm("Are you ready to start your intense psychological journey?");
+    alert("Good. Here goes.");
     FirstMorningBed();
+    
     
     
     //First Morning
@@ -71,12 +74,14 @@ function Game() {
             }
     */
     
+    //First Morning after dressed
     function FirstMorningUpstairsDressed() {
         alert("There are no other rooms open upstairs. You head downstairs.");
         FirstMorningDownstairsDressed();
     }
     
     
+    //First Morning after refusing dressed
     function FirstMorningUpstairsNaked() {
         FirstUpstairsPrompt();
         
@@ -136,6 +141,7 @@ function Game() {
     }
     
     
+    //First Breakfast after dressed
     function FirstMorningDownstairsDressed() {
         alert("Entering the kitchen, you see a plate of bacon and eggs on the counter. Your mother appears from behind the fridge and places a small jar next to the plate.");
         prompt("She says, \"Ah, " + playerName + "! You're finally awake! How are you feeling?\"");
@@ -145,10 +151,64 @@ function Game() {
         alert("\"Well, anyway...\"");
         alert("She gestures to the small jar.");
         alert("\"I managed to have these pills prescribed for you, they might help you sleep more soundly.\"");
-        var firstMorningDrug = prompt("Pills, or breakfast first? *This choice will matter* \n - Pills \n - Breakfast");
+        FirstMorningBreakfast();
+        
+        function FirstMorningBreakfast() {
+            var firstMorningDrug = prompt("Pills, or breakfast first? *This choice will matter* \n - Pills \n - Breakfast").toLowerCase();
+            
+            if(firstMorningDrug == "pills" || firstMorningDrug == "p") {
+                alert("You take the pills. Then you wolf down your breakfast.");
+                alert("Your mom gives you a disapproving look.");
+                alert("\"Head on back to bed.\"");
+                FirstMorningDrugSleep();
+                
+                function FirstMorningDrugSleep() {
+                    var firstMorningBackBedDrug = prompt("You head back upstairs where your bed looks very inviting. \n - Sleep").toLowerCase();
+                    
+                    if(firstMorningBackBedDrug == "sleep" || firstMorningBackBedDrug == "s") {
+                        alert("You body slam your bed, shaking the house.");
+                        alert("You begin to drift out of consciousness.");
+                        FirstDrugDream();
+                    }
+                    
+                    else {
+                        alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                        FirstMorningDrugSleep();
+                    }
+                }
+            }
+            
+            else if(firstMorningDrug == "breakfast" || firstMorningDrug == "b") {
+                alert("You wolf down your breakfast. Then you take your pills.");
+                alert("Your mom gives you a blank stare.");
+                alert("\"Head on back to bed.\"");
+                FirstMorningSleep();
+                
+                function FirstMorningSleep() {
+                    var firstMorningBackBed = prompt("You head back upstairs where your bed looks very inviting. \n - Sleep").toLowerCase();
+                    
+                    if(firstMorningBackBed == "sleep" || firstMorningBackBed == "s") {
+                        alert("You body slam your bed, shaking the house.");
+                        alert("You begin to drift out of consciousness.");
+                        FirstDream();
+                    }
+                    
+                    else {
+                        alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                        FirstMorningSleep();
+                    }
+                }
+            }
+            
+            else {
+                alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                FirstMorningBreakfast();
+            }
+        }
     }
     
     
+    //First Breakfast after refusing dressed
     function FirstMorningDownstairsNaked() {
         alert("Entering the kitchen, you see a plate of bacon and eggs on the counter. Your mother appears from behind the fridge and places a small jar next to the plate.");
         prompt("She says, startled, \"AH! " + playerName + "?! Where are your clothes?!\"");
@@ -158,6 +218,73 @@ function Game() {
         alert("\"Well, anyway...\"");
         alert("She gestures to the small jar.");
         alert("\"I managed to have these pills prescribed for you, they might help you sleep more soundly.\"");
-        var firstMorningDrug = prompt("Pills, or breakfast first? *This choice will matter* \n - Pills \n - Breakfast");
+        FirstMorningBreakfastNaked();
+        
+        function FirstMorningBreakfastNaked() {
+            var firstMorningDrugNaked = prompt("Pills, or breakfast first? *This choice will matter* \n - Pills \n - Breakfast");
+            
+            if(firstMorningDrugNaked == "pills" || firstMorningDrugNaked == "p") {
+                alert("You take the pills. Then you wolf down your breakfast.");
+                alert("Your mom gives you a disapproving look.");
+                alert("\"Head on back to bed.\"");
+                FirstMorningDrugSleepNaked();
+                
+                function FirstMorningDrugSleepNaked() {
+                    var firstMorningBackBedDrugNaked = prompt("You head back upstairs where your bed looks very inviting. \n - Sleep").toLowerCase();
+                    
+                    if(firstMorningBackBedDrugNaked == "sleep" || firstMorningBackBedDrugNaked == "s") {
+                        alert("You body slam your bed, shaking the house.");
+                        alert("You begin to drift out of consciousness.");
+                        FirstDrugDream();
+                    }
+                    
+                    else {
+                        alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                        FirstMorningDrugSleepNaked();
+                    }
+                }
+            }
+            
+            else if(firstMorningDrugNaked == "breakfast" || firstMorningDrugNaked == "b") {
+                alert("You wolf down your breakfast. Then you take your pills.");
+                alert("Your mom gives you a blank stare.");
+                alert("\"Head on back to bed.\"");
+                FirstMorningSleepNaked();
+                
+                function FirstMorningSleepNaked() {
+                    var firstMorningBackBedNaked = prompt("You head back upstairs where your bed looks very inviting. \n - Sleep").toLowerCase();
+                    
+                    if(firstMorningBackBedNaked == "sleep" || firstMorningBackBedNaked == "s") {
+                        alert("You body slam your bed, shaking the house.");
+                        alert("You begin to drift out of consciousness.");
+                        FirstDream();
+                    }
+                    
+                    else {
+                        alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                        FirstMorningSleepNaked();
+                    }
+                }
+            }
+            
+            else {
+                alert("You have chosen an answer that either leads to nothing or doesn't make sense. Please try again.");
+                FirstMorningBreakfastNaked();
+            }
+        }
+    }
+    
+    
+    
+    //First Dream from breakfast first
+    function FirstDream() {
+        alert("You are asleep.");
+        
+    }
+    
+    //First Dream from pills first
+    function FirstDrugDream() {
+        alert("You are asleep.");
+        
     }
 }
